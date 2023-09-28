08a92ed5735d20d7aa55188580a89de5b821ed35
ssarun9871 <ssarun9871@gmail.com>
2023-09-28 22:32:51 +0530

    using useRef hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,37 +1,42 @@
 import React from "react";
 import "./Login.css";
 import "../UI/Button.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 //In this component, we only take the user data and pass on to the function(onAddUser) present in App.js
 const Login = (props) => {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
+  const userName = useRef();
+  const userAge = useRef();
+  // const [name, setName] = useState("");
+  // const [age, setAge] = useState("");
 
-  const onNameChangeHandler = (event) => {
-    setName(event.target.value);
-  };
+  // const onNameChangeHandler = (event) => {
+  //   setName(event.target.value);
+  // };
 
-  const onAgeChangeHandler = (event) => {
-    setAge(event.target.value);
-  };
+  // const onAgeChangeHandler = (event) => {
+  //   setAge(event.target.value);
+  // };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
     const userData = {
-      name: name,
-      age: age,
+      name: userName.current.value,
+      age: userAge.current.value,
     };
     props.onAddUser(userData);
+
+    userName.current.value = '';
+    userAge.current.value ='';
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>Username</label>
-        <input type="text" onChange={onNameChangeHandler}></input>
+        <input type="text" ref={userName}></input>
         <label>Age(years)</label>
-        <input type="text" onChange={onAgeChangeHandler}></input>
+        <input type="text" ref={userAge}></input>
       </div>
       <button className="button">Add user</button>
     </form>
